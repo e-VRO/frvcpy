1010a8e0dea5490724344788d6aeadd512b2a9a9
Nick Kullman <nick.kullman@gmail.com>
2019-01-24 07:38:48 -0800

    move algorithm to separate module

--- frvcp_py/labelalgo/core.py
+++ frvcp_py/labelalgo/core.py
@@ -227,21 +227,3 @@ class FRVCPInstance(object):
   def __init__(self):
     # more TODO
     return
-
-class PCCMAlgorithm(object):
-  """The labeling algorithm to solve the FRVCP."""
-  # TODO move to separate class
-  def __init__(self, instance, init_soc, nodes, adjacency_list,
-    node_local_id_dep, node_local_id_arr, max_slope
-  ):
-    self.instance = instance
-    self.init_soc = init_soc
-    self.nodes = nodes
-    self.adjacency_list = adjacency_list
-    self.node_local_id_dep = node_local_id_dep
-    self.node_local_id_arr = node_local_id_arr
-    self.max_slope = max_slope
-
-  def run_multiobj_shortest_path_algo(self, dominance, stop_at_first):
-    #TODO
-    return
