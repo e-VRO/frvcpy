9db43e597bf79ab7444825ee50f6bb82b1ac32f3
Nick Kullman <nick.kullman@gmail.com>
2019-01-28 18:51:13 -0800

    fix instance filename

--- test.py
+++ test.py
@@ -2,13 +2,13 @@
 # instance_file = write_instance_file(self.stuff, instance_filename)
 
 # once it exists...
-instance_filename = "./la_instances/instance1.json"
+instance_filename = "./instances/LAInstance1.json"
 route= [0,1,2,3]
 
 from frvcp_py.labelalgo.solver import Solver
 solver = Solver(instance_filename,route)
 
-e_feas_route = solver.solve()
+energy_feasible_route = solver.solve()
 
 # node = Node(1,"first node",NodeType.DEPOT)
 # node2 = Node(2,"second node",NodeType.CUSTOMER)
