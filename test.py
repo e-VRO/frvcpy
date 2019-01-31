05ee6790b0988b61f681144a9c0bda41a92a3a73
Nick Kullman <nick.kullman@gmail.com>
2019-01-31 12:06:50 -0800

    Add and test new instance
    And minor formatting improvements to test script

--- test.py
+++ test.py
@@ -1,13 +1,16 @@
-# TODO write instance file if it does not already exist
-# instance_file = write_instance_file(self.stuff, instance_filename)
+# specify instance and route
+instance_filename = "./instances/LAInstance-c10s2l2dT4.json"
+import random
+cust_seq = list(range(1,10))
+random.shuffle(cust_seq)
+route=[0]+cust_seq+[0]
+print(f"Initial route:\n{route}")
 
-# once it exists...
-instance_filename = "./instances/LAInstance2.json"
-route= [0,1,2,0]
-
+# intialize solver
 from frvcp_py.labelalgo.solver import Solver
 solver = Solver(instance_filename,route)
 
+# run solver, get output
 obj,route = solver.solve()
-print(f'Objective:{obj:.4}')
-print(f'Route:\n{route}')
+print(f'Objective:\n{obj:.4}')
+print(f'Energy-feasible route:\n{route}')
