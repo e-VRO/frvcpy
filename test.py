b1809cebdbfc915b7799b4081387804b0111ab9f
Nick Kullman <nick.kullman@gmail.com>
2019-01-31 17:43:55 -0800

    Add two larger test instances

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 # specify instance and route
-instance_filename = "./instances/LAInstance-c20s3l4dT2.json"
+instance_filename = "./instances/LAInstance-c40s5l4dT5.json"
+# generate random fixed route over the customer set
 import random
-cust_seq = list(range(1,20))
+cust_seq = list(range(1,40))
 random.shuffle(cust_seq)
 route=[0]+cust_seq+[0]
 print(f"Initial route:\n{route}")
