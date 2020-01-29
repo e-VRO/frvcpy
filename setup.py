7b6e198387cc121e9fab40ddd0cfc8f98ac50804
Nick Kullman <Nick.Kullman@gmail.com>
2020-01-28 17:46:52 -0800

    Add relevant content to setup

--- setup.py
+++ setup.py
@@ -1,4 +1,29 @@
-from setuptools import setup
+import setuptools
 
-setup(name='frvcp_py',
-      version='0.0.1')
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+      name='frvcpy-nkullman',
+      version='0.0.1',
+      author='Nicholas Kullman',
+      author_email='nicholas.kullman@etu.univ-tours.fr',
+      description='A solver for fixed route vehicle charging problems',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      url='https://github.com/e-VRO/frvcpy',
+      packages=setuptools.find_packages(),
+      license='Apache',
+      classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: Apache License",
+            "Operating System :: OS Independent",
+      ],
+      python_requires='>=3.6',
+      entry_points={
+            'console_scripts': [
+                  'frvcpy=frvcpy.solver:main',
+                  'frvcpy-translate=frvcpy.translator:main',
+            ],
+      }
+)
