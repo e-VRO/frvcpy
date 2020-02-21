f8441384943deedd5a1502231a247dad1b600f40
Nick Kullman <Nick.Kullman@gmail.com>
2020-02-20 16:47:16 -0800

    Update version
    Ready for first release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='frvcpy',
-    version='0.1.0rc5',
+    version='0.1.0',
     author='Nicholas Kullman',
     author_email='nicholas.kullman@etu.univ-tours.fr',
     description='A solver for fixed route vehicle charging problems',
